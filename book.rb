745184133b3ef3912f3c1b9333f09225e7c5f14f
codedit334 <oussama7elabdioui@gmail.com>
2023-05-16 19:50:57 +0100

    Add update Student class

--- book.rb
+++ book.rb
@@ -1,3 +1,6 @@
+require_relative 'person.rb'
+require_relative 'rental.rb'
+
 class Book
     attr_accessor :title, :author
   
@@ -6,4 +9,20 @@ class Book
       @author = author
     end
   end
+  
+  book1 = Book.new("The Great Gatsby", "F. Scott Fitzgerald")
+  book2 = Book.new("To Kill a Mockingbird", "Harper Lee")
+  
+  person1 = Person.new("Alice")
+  person2 = Person.new("Bob")
+  
+  rental1 = Rental.new(book1, person1, Date.today)
+  rental2 = Rental.new(book1, person2, Date.today - 7)
+  rental3 = Rental.new(book2, person2, Date.today - 14)
+  
+  puts book1.rentals.map { |rental| rental.person.name }.join(", ")
+  # Output: Alice, Bob
+  
+  puts person2.rentals.map { |rental| rental.book.title }.join(", ")
+  # Output: The Great Gatsby, To Kill a Mockingbird
   
--- student.rb
+++ student.rb
@@ -1,9 +1,13 @@
 require_relative 'person'
 
 class Student < Person
+  attr_accessor :name, :classroom
+
   def initialize(age, classroom, name: 'Unknown', parent_permission: true)
     super(age, name: name, parent_permission: parent_permission)
     @classroom = classroom
+    @name = name
+    @classroom = nil
   end
 
   def play_hooky
