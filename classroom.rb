bf4105145630e5fda45ed86d9b3bf9ba35f1d142
codedit334 <oussama7elabdioui@gmail.com>
2023-05-16 20:40:52 +0100

    Run rubocop

--- classroom.rb
+++ classroom.rb
@@ -1,24 +1,23 @@
-require_relative 'student.rb'
+require_relative 'student'
 
 class Classroom
-    attr_accessor :label
-    attr_reader :students
-  
-    def initialize(label)
-      @label = label
-      @students = []
-    end
-  
-    def add_student(student)
-      @students << student
-      student.classroom = self
-    end
+  attr_accessor :label
+  attr_reader :students
+
+  def initialize(label)
+    @label = label
+    @students = []
   end
 
-classroom = Classroom.new("Math")
+  def add_student(student)
+    @students << student
+    student.classroom = self
+  end
+end
+
+classroom = Classroom.new('Math')
 student1 = Student.new(11, nil, name: 'Alice')
 student2 = Student.new(14, nil, name: 'Bob')
 classroom.add_student(student1)
 classroom.add_student(student2)
-p classroom.students.map { |student| student.name }
-  
+p classroom.students.map(&:name)
