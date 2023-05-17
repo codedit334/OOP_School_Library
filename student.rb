990a40d30dbabbe4f73e81c458c7b65ee15bd651
codedit334 <oussama7elabdioui@gmail.com>
2023-05-17 13:29:25 +0100

    Run rubocop

--- student.rb
+++ student.rb
@@ -1,7 +1,8 @@
 require_relative 'person'
 
 class Student < Person
-  attr_accessor :name, :classroom
+  attr_accessor :name
+  attr_reader :classroom
 
   def initialize(age, classroom, name: 'Unknown', parent_permission: true)
     super(age, name: name, parent_permission: parent_permission)
@@ -15,5 +16,5 @@ class Student < Person
   def classroom=(classroom)
     @classroom = classroom
     classroom.students.push(self) unless classroom.students.include?(self)
-end
+  end
 end
