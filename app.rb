7e2edb2ce5779cbf03008346e9b687b986d4896b
codedit334 <oussama7elabdioui@gmail.com>
2023-05-18 13:29:56 +0100

    INITIALIZE APP.RB

---
+++ app.rb
@@ -0,0 +1,17 @@
+require_relative 'book'
+require_relative 'person'
+require_relative 'teacher'
+require_relative 'student'
+require_relative 'rental'
+
+class App
+
+  def initialize
+    @books = []
+    @people = []
+    @rentals = []
+  end
+
+  
+
+end
