faaae6e2ff99aa9820cdf1ba702dfbfe5addd6f4
codedit334 <oussama7elabdioui@gmail.com>
2023-05-18 13:33:14 +0100

    Create a reantal method

--- app.rb
+++ app.rb
@@ -87,7 +87,30 @@ class App
     puts 'Book created successfully'
   end
 
-  
+  # Create a rental
+  def create_rental
+    puts 'Select a book from the following list by number'
+    @books.each_with_index do |book, index|
+      puts "#{index}) Title: #{book['title']}, Author: #{book['author']}"
+    end
+
+    book_index = gets.chomp.to_i
+
+    puts 'Select a person from the following list by number (not ID)'
+
+    @people.each_with_index do |person, index|
+      puts "#{index}) Name: #{person['name']}, ID: #{person['id']}, Age: #{person['age']}"
+    end
+
+    person_index = gets.chomp.to_i
+
+    print 'Date: '
+    date = gets.chomp
+
+    @rentals << Rental.new(date, @books[book_index], @people[person_index])
+    puts 'Rental created successfully'
+  end
+end
   
 
 end
